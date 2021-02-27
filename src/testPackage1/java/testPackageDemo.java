b5a444288efcc329487038eaa4e0c3dba5f78dfd
kobe_24o <mingmingchen1989@163.com>
2021-02-27 20:51:52 +0800

    Update testPackageDemo.java

--- src/testPackage1/java/testPackageDemo.java
+++ src/testPackage1/java/testPackageDemo.java
@@ -1,11 +1,11 @@
-package testPackage.java;
-class testPackage {
+package testPackage1.java;
+class testPg {
     public String talk(){
         return "talk()";
     }
 }
 class testAnother{
     public static void main(String[] args){
-        System.out.println(new testPackage().talk());
+        System.out.println(new testPg().talk());
     }
 }
